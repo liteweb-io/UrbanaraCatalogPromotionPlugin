2de4e52c755bdc9d1f0cbdde8c535b193d1efd29
Łukasz <lchrusciel@gmail.com>
2017-03-08 16:25:28 +0100

    [Resource] Move resource configuration to configuration tree

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,6 +2,11 @@
 
 namespace Acme\SyliusCatalogPromotionBundle\DependencyInjection;
 
+use Acme\SyliusCatalogPromotionBundle\Entity\CatalogPromotion;
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Bundle\ResourceBundle\Form\Type\DefaultResourceType;
+use Sylius\Component\Resource\Factory\Factory;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -15,6 +20,31 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('sylius_catalog_promotion');
 
+        $this->addCatalogPromotion($rootNode->children());
+
         return $treeBuilder;
+    }
+
+    /**
+     * @param NodeBuilder $rootNodeBuilder
+     */
+    private function addCatalogPromotion(NodeBuilder $rootNodeBuilder)
+    {
+        $rootNodeBuilder
+            ->arrayNode('resources')
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->arrayNode('catalog_promotion')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('classes')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue(CatalogPromotion::class)->cannotBeEmpty()->end()
+                                ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
+                                ->scalarNode('repository')->cannotBeEmpty()->end()
+                                ->scalarNode('form')->defaultValue(DefaultResourceType::class)->cannotBeEmpty()->end()
+                                ->scalarNode('factory')->defaultValue(Factory::class)->cannotBeEmpty()->end()
+        ;
     }
 }
--- src/DependencyInjection/SyliusCatalogPromotionExtension.php
+++ src/DependencyInjection/SyliusCatalogPromotionExtension.php
@@ -2,12 +2,14 @@
 
 namespace Acme\SyliusCatalogPromotionBundle\DependencyInjection;
 
+use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
+use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class SyliusCatalogPromotionExtension extends Extension
+final class SyliusCatalogPromotionExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -17,6 +19,17 @@ final class SyliusCatalogPromotionExtension extends Extension
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
+        $this->registerResources('acme_sylius_catalog_promotion', SyliusResourceBundle::DRIVER_DOCTRINE_ORM, $config['resources'], $container);
+
         $loader->load('services.xml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration($this->getConfiguration([], $container), $container->getExtensionConfig($this->getAlias()));
+        $this->registerResources('acme_sylius_catalog_promotion', SyliusResourceBundle::DRIVER_DOCTRINE_ORM, $config['resources'], $container);
     }
 }
