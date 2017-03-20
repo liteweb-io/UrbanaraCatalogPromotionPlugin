cc4462289e818616529df07612ea258c51b72859
Łukasz <lchrusciel@gmail.com>
2017-03-20 11:36:09 +0100

    [Catalog] Usage fixed form types

--- src/Form/Type/Action/FixedCatalogDiscountType.php
+++ src/Form/Type/Action/FixedCatalogDiscountType.php
@@ -2,62 +2,39 @@
 
 namespace Acme\SyliusCatalogPromotionPlugin\Form\Type\Action;
 
+use Sylius\Bundle\CoreBundle\Form\Type\ChannelCollectionType;
 use Sylius\Bundle\MoneyBundle\Form\Type\MoneyType;
-use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\GreaterThan;
 
 final class FixedCatalogDiscountType extends AbstractType
 {
-    /**
-     * @var ChannelRepositoryInterface
-     */
-    private $channelRepository;
-
-    /**
-     * @param ChannelRepositoryInterface $channelRepository
-     */
-    public function __construct(ChannelRepositoryInterface $channelRepository)
-    {
-        $this->channelRepository = $channelRepository;
-    }
-
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('values', null, [
+        $builder->add('values', ChannelCollectionType::class, [
             'label' => 'sylius.form.variant.price',
-            'compound' => true,
-        ]);
-
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options) {
-            $form = $event->getForm()->get('values');
-
-            /** @var ChannelInterface[] $channels */
-            $channels = $this->channelRepository->findAll();
-            foreach ($channels as $channel) {
-                $form->add($channel->getCode(), MoneyType::class, [
-                    'property_path' => '[' . $channel->getCode() . ']',
-                    'required' => false,
-                    'block_name' => 'entry',
-                    'currency' => $channel->getBaseCurrency()->getCode(),
+            'entry_type' => MoneyType::class,
+            'entry_options' => function (ChannelInterface $channel) {
+                return [
                     'label' => $channel->getName(),
+                    'currency' => $channel->getBaseCurrency()->getCode(),
+                    'required' => false,
                     'constraints' => [
-                        new Range([
+                        new GreaterThan([
                             'groups' => ['sylius'],
-                            'min' => 0,
-                            'minMessage' => 'acme_sylius_catalog_promotion.catalog_promotion.configuration.discounts.min',
+                            'value' => 0,
+                            'message' => 'acme_sylius_catalog_promotion.catalog_promotion.configuration.discounts.min',
                         ]),
                     ],
-                ]);
-            }
-        });
+                ];
+            },
+            'required' => false,
+        ]);
     }
 
     /**
