8d582e91763c4d19825895b8589548608fa6fc28
Łukasz <lchrusciel@gmail.com>
2017-03-24 10:02:33 +0100

    [Catalog] Use default sylius product autocomplete

--- src/Form/Type/Rule/IsProductType.php
+++ src/Form/Type/Rule/IsProductType.php
@@ -2,12 +2,10 @@
 
 namespace Acme\SyliusCatalogPromotionPlugin\Form\Type\Rule;
 
-use Mockery\Matcher\Type;
-use Sylius\Bundle\ResourceBundle\Form\Type\ResourceAutocompleteChoiceType;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductAutocompleteChoiceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 final class IsProductType extends AbstractType
 {
@@ -30,12 +28,9 @@ final class IsProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('products', ResourceAutocompleteChoiceType::class, [
+            ->add('products', ProductAutocompleteChoiceType::class, [
                 'label' => 'acme_sylius_catalog_promotion.form.catalog_promotion_rule.products',
-                'resource' => 'sylius.product',
-                'choice_name' => 'name',
                 'multiple' => true,
-                'choice_value' => 'code',
             ])
         ;
 
