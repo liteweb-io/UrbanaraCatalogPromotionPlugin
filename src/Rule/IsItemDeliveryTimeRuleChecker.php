d959b11a03840495ec50a6dd111da90378140fc5
Manuel Gomez <manuel.gomez@urbanara.com>
2017-05-16 10:03:27 +0200

    ITK-484 Promotions: New rule for Wait & Save FIX -Product attribute ETA is stored in a static datetime in field ETA_date. -Catalog Wait and save promo compares ETA date with interval based on current date plus number of defined weeks.

--- src/Rule/IsItemDeliveryTimeRuleChecker.php
+++ src/Rule/IsItemDeliveryTimeRuleChecker.php
@@ -11,20 +11,20 @@ final class IsItemDeliveryTimeRuleChecker implements RuleCheckerInterface
 {
     const TYPE = 'is_product_delivery_time_in_scope';
 
-    const PRODUCT_ATTRIBUTE_DELIVERY_TIME = 'eta';
+    const PRODUCT_ATTRIBUTE_DELIVERY_TIME = 'eta_date';
 
     const CRITERIA_MORE = 'more';
     const CRITERIA_LESS = 'less';
     const CRITERIA_EQUAL = 'equal';
 
-    const ERROR_MSG_ETA_NOT_FOUND = "ETA not found for product %s";
+    const ERROR_MSG_ETA_NOT_FOUND = "ETA date invalid or not found for product %s";
     const ERROR_MSG_NO_CRITERIA_OR_WEEKS_NUMBER_ITEMS_FOUND =
         "Wrong rule configuration. No criteria or weeks number items found";
 
     /**
      * {@inheritdoc}
      */
-    public function getConfigurationFormType()
+    public function getConfigurationFormType() : string
     {
         return IsItemDeliveryTimeType::class;
     }
@@ -32,7 +32,7 @@ final class IsItemDeliveryTimeRuleChecker implements RuleCheckerInterface
     /**
      * {@inheritdoc}
      */
-    public function isEligible(ProductVariantInterface $productVariant, array $configuration)
+    public function isEligible(ProductVariantInterface $productVariant, array $configuration) : bool
     {
         $etaAttribute = $productVariant->getProduct()->getAttributeByCodeAndLocale(
             self::PRODUCT_ATTRIBUTE_DELIVERY_TIME
@@ -47,67 +47,87 @@ final class IsItemDeliveryTimeRuleChecker implements RuleCheckerInterface
             );
         }
 
-        $etaInWeeks = intval(ceil($etaAttribute->getValue() / 7));
+        $etaParsedDate = \DateTime::createFromFormat(DATE_ISO8601, $etaAttribute->getValue());
+
+        if (!$etaParsedDate || !($etaParsedDate instanceof \DateTime)) {
+            throw new CatalogPromotionRuleException(self::ERROR_MSG_ETA_NOT_FOUND);
+        }
 
         if (!isset($configuration['criteria']) || !isset($configuration['weeks'])) {
             throw new CatalogPromotionRuleException(self::ERROR_MSG_NO_CRITERIA_OR_WEEKS_NUMBER_ITEMS_FOUND);
         }
 
-        return $this->validateElegibility($configuration['criteria'], $etaInWeeks, $configuration['weeks']);
+        return $this->validateElegibility($configuration['criteria'], $configuration['weeks'], $etaParsedDate);
     }
 
     /**
-     * @param string $criteria
-     * @param int    $etaInWeeks
-     * @param int    $referenceDeliveryWeeks
+     * @param string    $criteria
+     * @param \DateTime $etaDate
+     * @param int       $referenceDeliveryWeeks
      *
      * @return bool
      */
-    private function validateElegibility(string $criteria, int $etaInWeeks, int $referenceDeliveryWeeks)
+    private function validateElegibility(string $criteria, int $referenceDeliveryWeeks, \DateTime $etaDate) : bool
     {
         if ($criteria == self::CRITERIA_MORE) {
-            return $this->isGreaterThan($etaInWeeks, $referenceDeliveryWeeks);
+            return $this->isGreaterThan($etaDate, $referenceDeliveryWeeks);
         }
         if ($criteria == self::CRITERIA_EQUAL) {
-            return $this->isEqualsTo($etaInWeeks, $referenceDeliveryWeeks);
+            return $this->isEqualsTo($etaDate, $referenceDeliveryWeeks);
         }
         if ($criteria == self::CRITERIA_LESS) {
-            return $this->isLessThan($etaInWeeks, $referenceDeliveryWeeks);
+            return $this->isLessThan($etaDate, $referenceDeliveryWeeks);
         }
 
         return false;
     }
 
     /**
-     * @param int $etaInWeeks
-     * @param int $referenceDeliveryWeeks
+     * @param \DateTime $etaDate
+     * @param int       $referenceDeliveryWeeks
      *
      * @return bool
      */
-    private function isGreaterThan(int $etaInWeeks, int $referenceDeliveryWeeks)
+    private function isGreaterThan(\DateTime $etaDate, int $referenceDeliveryWeeks) : bool
     {
-        return $etaInWeeks > $referenceDeliveryWeeks;
+        return $etaDate > $this->getDateTimeNumWeeksFromNow($referenceDeliveryWeeks);
     }
 
     /**
-     * @param int $etaInWeeks
-     * @param int $referenceDeliveryWeeks
+     * @param \DateTime $etaDate
+     * @param int       $referenceDeliveryWeeks
      *
      * @return bool
      */
-    private function isEqualsTo(int $etaInWeeks, int $referenceDeliveryWeeks)
+    private function isEqualsTo(\DateTime $etaDate, int $referenceDeliveryWeeks) : bool
     {
-        return $etaInWeeks == $referenceDeliveryWeeks;
+        return $etaDate === $this->getDateTimeNumWeeksFromNow($referenceDeliveryWeeks);
     }
 
     /**
-     * @param int $etaInWeeks
-     * @param int $referenceDeliveryWeeks
+     * @param \DateTime $etaDate
+     * @param int       $referenceDeliveryWeeks
      *
      * @return bool
      */
-    private function isLessThan(int $etaInWeeks, int $referenceDeliveryWeeks)
+    private function isLessThan(\DateTime $etaDate, int $referenceDeliveryWeeks) : bool
     {
-        return $etaInWeeks < $referenceDeliveryWeeks;
+        return $etaDate < $this->getDateTimeNumWeeksFromNow($referenceDeliveryWeeks);
+    }
+
+    /**
+     * @param int $numWeeks
+     *
+     * @return \DateTime
+     */
+    private function getDateTimeNumWeeksFromNow(int $numWeeks) : \DateTime
+    {
+        $now = (new \DateTime())->setTime(0, 0, 0, 0);
+
+        if ($numWeeks <= 0) {
+            return $now;
+        }
+
+        return date_add($now, new \DateInterval("P{$numWeeks}W"));
     }
 }
