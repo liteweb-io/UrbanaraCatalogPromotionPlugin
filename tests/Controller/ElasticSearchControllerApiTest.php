e3afc0e5f40d45d47b7dc5c77930bf82b0942238
Kamil Kokot <kamil@kokot.me>
2017-07-14 16:47:23 +0200

    Rename ElasticSearch-related fixtures

--- tests/Controller/ElasticSearchControllerApiTest.php
+++ tests/Controller/ElasticSearchControllerApiTest.php
@@ -7,20 +7,20 @@ namespace Tests\Sylius\UrbanaraCatalogPromotionPlugin\Controller;
 use Lakion\ApiTestCase\JsonApiTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
-final class SearchControllerApiTest extends JsonApiTestCase
+final class ElasticSearchControllerApiTest extends JsonApiTestCase
 {
     /**
      * @test
      */
     public function it_shows_paginated_product_list()
     {
-        $this->loadFixturesFromFile('shop.yml');
+        $this->loadFixturesFromFile('search_shop.yml');
 
         $this->client->request('GET', '/shop-api/products', [], [], ['ACCEPT' => 'application/json'], '{}');
 
         $response = $this->client->getResponse();
 
-        $this->assertResponse($response, 'product_list_page', Response::HTTP_OK);
+        $this->assertResponse($response, 'elastic_search/product_list_page', Response::HTTP_OK);
     }
 
     /**
