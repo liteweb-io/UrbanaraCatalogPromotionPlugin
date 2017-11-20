2fa7fcd7a4d4bc71117a87aff92c36ad1c10bcda
Manuel Gomez <manuel.gomez@urbanara.com>
2017-11-20 17:17:32 +0100

    commenting out unused stuff

--- src/DependencyInjection/Compiler/RegisterCatalogPromotionActionsPass.php
+++ src/DependencyInjection/Compiler/RegisterCatalogPromotionActionsPass.php
@@ -36,6 +36,5 @@ final class RegisterCatalogPromotionActionsPass implements CompilerPassInterface
         }
 
         $container->setParameter('urbanara_catalog_promotion.catalog_promotion_actions', $actions);
-//        $container->setParameter('urbanara_catalog_promotion.catalog_promotion_actions',  ['custom' => 'Label']);
     }
 }
